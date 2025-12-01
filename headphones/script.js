47a46fe3c03ff73d64bbf2f2610e7fd4a69c7d77
Abubakar Sadiq Ibrahim <iabu6133@gmail.com>
2025-12-01 21:20:32 +0000

    Added all features from question prompts

---
+++ headphones/script.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navbar = document.querySelector('.navbar');
+
+    menuToggle.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+
+        const icon = menuToggle.querySelector('i');
+        if (navbar.classList.contains('active')) {
+            icon.classList.remove('fa-bars');
+            icon.classList.add('fa-times');
+        } else {
+            icon.classList.remove('fa-times');
+            icon.classList.add('fa-bars');
+        }
+    });
+});
